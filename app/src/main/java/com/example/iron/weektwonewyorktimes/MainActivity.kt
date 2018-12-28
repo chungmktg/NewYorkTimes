c6bed21ebdd605939078b846dca2d971cb790e0f
chungmktg <chungmktg@gmail.com>
2018-12-28 15:45:11 +0700

    webview1

--- app/src/main/java/com/example/iron/weektwonewyorktimes/MainActivity.kt
+++ app/src/main/java/com/example/iron/weektwonewyorktimes/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.iron.weektwonewyorktimes
 
+import android.content.Context
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
@@ -16,19 +17,29 @@ import com.example.iron.weektwonewyorktimes.Presenters.MainPresenter
 import com.example.iron.weektwonewyorktimes.Views.FilterFragment
 import com.example.iron.weektwonewyorktimes.Views.IView
 import kotlinx.android.synthetic.main.activity_main.*
+import android.support.v7.widget.RecyclerView
+import android.widget.Toast
+import com.example.iron.weektwonewyorktimes.Models.EndlessRecyclerViewScrollListener
+import android.net.NetworkInfo
+import android.content.Context.CONNECTIVITY_SERVICE
+import android.support.v4.content.ContextCompat.getSystemService
+import android.net.ConnectivityManager
+import android.support.v4.content.ContextCompat
+import java.io.IOException
+
 
 class MainActivity : AppCompatActivity(), IView {
     lateinit var mainPresenter: MainPresenter
     private lateinit var adapters: ArticleAdapter
     lateinit var filterPresenter: FiterPresenter
 
-
+    private var scrollListener: EndlessRecyclerViewScrollListener? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
+        checkInternet()
         filterPresenter =  FiterPresenter(this)
         mainPresenter= MainPresenter(this)
         mainPresenter.loadData()
@@ -36,19 +47,44 @@ class MainActivity : AppCompatActivity(), IView {
 
     }
 
+    private fun checkInternet() {
+        if (!isNetworkAvailable()) {
+            Toast.makeText(getApplicationContext(), "Opps looks like " +
+                    "network connectivity problem. Turn on Internet and click Refresh in menu option",
+                Toast.LENGTH_LONG).show();
+        }
+
+        if (!isOnline()) {
+            Toast.makeText(getApplicationContext(), "Your device is not online, " +
+                    "check wifi and try again!",
+                Toast.LENGTH_LONG).show();
+        }
+    }
+
     override fun getDataSuccess(listDemo: List<Doc>?) {
         adapters = ArticleAdapter(this@MainActivity, listDemo)
-        val manager =  StaggeredGridLayoutManager(3, LinearLayoutManager.VERTICAL)
+        val manager = StaggeredGridLayoutManager(3, LinearLayoutManager.VERTICAL)
         recycleview_main.layoutManager = manager
         recycleview_main.setHasFixedSize(true)
         recycleview_main.adapter = adapters
         adapters.notifyDataSetChanged()
+        manager.setGapStrategy(StaggeredGridLayoutManager.GAP_HANDLING_NONE)
+        recycleview_main.addOnScrollListener(
+            object : EndlessRecyclerViewScrollListener(manager) {
+                override fun onLoadMore(page: Int, totalItemsCount: Int, view: RecyclerView) {
+                    loadNextDataFromApi(page)
+                }
+            })
+
+    }
+
+    private fun loadNextDataFromApi(page: Int) {
+        Toast.makeText(this, "Loading more..."+ page, Toast.LENGTH_SHORT).show()
     }
 
     override fun getDataFailed(message: String) {
         Log.d("testfail",message)
     }
-
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         val inflater: MenuInflater = menuInflater
         inflater.inflate(R.menu.menu_main, menu)
@@ -66,12 +102,8 @@ class MainActivity : AppCompatActivity(), IView {
                 return false
             }
         })
-
         return true
     }
-
-
-
     override fun onOptionsItemSelected(item: MenuItem?): Boolean {
         when(item?.itemId){
             R.id.filter_menu -> {
@@ -79,10 +111,33 @@ class MainActivity : AppCompatActivity(), IView {
                 val filterFragment = FilterFragment()
                 filterFragment.show(fm,"filter")
             }
-
-
+            R.id.refresh -> {
+                mainPresenter.loadData()
+            }
         }
         return super.onOptionsItemSelected(item)
     }
+
+    private fun isNetworkAvailable(): Boolean {
+        val connectivityManager = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val activeNetworkInfo = connectivityManager.activeNetworkInfo
+        return activeNetworkInfo != null && activeNetworkInfo.isConnectedOrConnecting
+    }
+
+    private fun isOnline(): Boolean {
+        val runtime = Runtime.getRuntime()
+        try {
+            val ipProcess = runtime.exec("/system/bin/ping -c 1 8.8.8.8")
+            val exitValue = ipProcess.waitFor()
+            return exitValue == 0
+        } catch (e: InterruptedException) {
+            e.printStackTrace()
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+
+        return false
+    }
+
 
 }
--- app/src/main/java/com/example/iron/weektwonewyorktimes/Presenters/MainPresenter.kt
+++ app/src/main/java/com/example/iron/weektwonewyorktimes/Presenters/MainPresenter.kt
@@ -1,10 +1,16 @@
 package com.example.iron.weektwonewyorktimes.Presenters
 
+import android.content.Context
+import android.net.ConnectivityManager
 import android.util.Log
 import com.example.iron.weektwonewyorktimes.Models.Doc
 import com.example.iron.weektwonewyorktimes.Models.GetdataAPI
 import com.example.iron.weektwonewyorktimes.Models.IModelsGetDataAPI
 import com.example.iron.weektwonewyorktimes.Views.IView
+import java.io.IOException
+import android.net.NetworkInfo
+import android.support.v4.content.ContextCompat.*
+
 
 class MainPresenter: IModelsGetDataAPI {
     private var iView: IView?=null
@@ -21,6 +27,9 @@ class MainPresenter: IModelsGetDataAPI {
     fun getFilterSeach(string: String?){
         getdataAPI.getRetrofitFromSeach(string)
     }
+    fun checkInternet(){
+
+    }
     override fun getSucces(doclist: List<Doc>?) {
         Log.d("mainpresentergetsuccess",doclist.toString())
         iView?.getDataSuccess(doclist)
@@ -29,5 +38,6 @@ class MainPresenter: IModelsGetDataAPI {
     override fun getFailed(message: String) {
         iView?.getDataFailed(message)
     }
+
 
 }
